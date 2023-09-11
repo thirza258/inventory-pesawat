6659a7daf5b87e16393829a5d634c5fc8958271d
Thirza <thirzahmad@gmail.com>
2023-09-11 15:58:29 +0700

    feat: make item model and inventory project

--- main/views.py
+++ main/views.py
@@ -5,6 +5,20 @@ def main_view(request):
     context = {
         "Nama_Aplikasi": "Inventory RPG",
         "Nama": "Thirza Ahmad Tsaqif",
-        "Class": "PBP E"
+        "Class": "PBP E",
+        "Inventory" : [
+            {"name": "Boeing 737",
+             "amount": "10",
+             "description": "Pesawat Komersial Boeing 737",
+             "engine": "Rolls Royce",
+             "winglet": "True"
+             },
+            {"name": "Boeing 747",
+             "amount": "10",
+             "description": "Pesawat Komersial Boeing 747",
+             "engine": "4",
+             "winglet": "True"
+             }
+        ]
     }
     return render(request, "main.html", context)
