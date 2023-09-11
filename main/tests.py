5135a13cbdf71c4a068c7ef77dfe20aa3436ce91
Thirza <thirzahmad@gmail.com>
2023-09-11 16:13:17 +0700

    refactor: restart adaptable

--- main/tests.py
+++ main/tests.py
@@ -1,23 +1,23 @@
 from django.test import TestCase, Client
 
 # Create your tests here.
-class mainTest(TestCase):
-    def test_main_url_is_exist(self):
-        response = Client().get('/main/')
-        self.assertEqual(response.status_code, 200)
+# class mainTest(TestCase):
+#     def test_main_url_is_exist(self):
+#         response = Client().get('/main/')
+#         self.assertEqual(response.status_code, 200)
     
-    def test_main_using_main_template(self):
-        response = Client().get('/main/')
-        self.assertTemplateUsed(response, 'main.html')
+#     def test_main_using_main_template(self):
+#         response = Client().get('/main/')
+#         self.assertTemplateUsed(response, 'main.html')
 
-    def test_context_nama_is_right(self):
-        response = Client().get('/main/')
-        self.assertEqual(response.context['Nama'], 'Thirza Ahmad Tsaqif')
+#     def test_context_nama_is_right(self):
+#         response = Client().get('/main/')
+#         self.assertEqual(response.context['Nama'], 'Thirza Ahmad Tsaqif')
 
-    def test_context_class_is_right(self):
-        response = Client().get('/main/')
-        self.assertEqual(response.context['Class'], 'PBP E')
+#     def test_context_class_is_right(self):
+#         response = Client().get('/main/')
+#         self.assertEqual(response.context['Class'], 'PBP E')
     
-    def test_context_nama_aplikasi_is_right(self):
-        response = Client().get('/main/')
-        self.assertEqual(response.context['Nama_Aplikasi'], 'Inventory RPG')
+#     def test_context_nama_aplikasi_is_right(self):
+#         response = Client().get('/main/')
+#         self.assertEqual(response.context['Nama_Aplikasi'], 'Inventory RPG')
--- main/views.py
+++ main/views.py
@@ -1,24 +1,24 @@
 from django.shortcuts import render
 
 # Create your views here.
-def main_view(request):
-    context = {
-        "Nama_Aplikasi": "Inventory RPG",
-        "Nama": "Thirza Ahmad Tsaqif",
-        "Class": "PBP E",
-        "Inventory" : [
-            {"name": "Boeing 737",
-             "amount": "10",
-             "description": "Pesawat Komersial Boeing 737",
-             "engine": "Rolls Royce",
-             "winglet": "True"
-             },
-            {"name": "Boeing 747",
-             "amount": "10",
-             "description": "Pesawat Komersial Boeing 747",
-             "engine": "4",
-             "winglet": "True"
-             }
-        ]
-    }
-    return render(request, "main.html", context)
+# def main_view(request):
+    # context = {
+    #     "Nama_Aplikasi": "Inventory Pesawat",
+    #     "Nama": "Thirza Ahmad Tsaqif",
+    #     "Class": "PBP E",
+    #     "Inventory" : [
+    #         {"name": "Boeing 737 - 900",
+    #          "amount": "10",
+    #          "description": "Pesawat Komersial Boeing 737 - 900 yang diluncurkan pada tahun 1997",
+    #          "engine": "Rolls Royce",
+    #          "winglet": "True"
+    #          },
+    #         {"name": "Boeing 747 - 400",
+    #          "amount": "10",
+    #          "description": "Pesawat Komersial Boeing 747 - 400 yang diluncurkan pada tahun 1988",
+    #          "engine": "4",
+    #          "winglet": "False"
+    #          }
+    #     ]
+    # }
+    # return render(request, "main.html", context)
