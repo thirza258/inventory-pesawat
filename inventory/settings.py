8523f41044347e05e76130b231a4c8e2775c76c0
Thirza <thirzahmad@gmail.com>
2023-10-04 00:14:29 +0700

    pengerjaan desain pertama Tugas 5 bagian register tambah, dan login

--- inventory/settings.py
+++ inventory/settings.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     "main",
 ]
 
+
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
@@ -127,3 +128,4 @@ STATIC_ROOT = os.path.join(BASE_DIR, "static")
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
